cd44c36c9e155081ccf68d06b76dab00b70da165
JoshHarris85 <joshua.harris1085@gmail.com>
2019-04-05 16:33:31 -0500

    Lesson 12

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import { TweenMax } from "gsap"
 
-const box = document.createElement("div");
-box.setAttribute("class", "box");
-document.body.appendChild(box);
-
-TweenMax.set(box, { transformPerspective: 600 });
-
-box.addEventListener("click", () => {
-  TweenMax.to(box, 1, { rotationY: "+=180" });
+document.addEventListener("mousemove", event => {
+  TweenMax.set(document.body, { perspective: event.x });
 });
+
+Array.from({ length: 30 })
+  .map(() => document.createElement("div"))
+  .forEach(box => {
+    box.setAttribute("class", "box");
+    document.body.appendChild(box);
+
+    TweenMax.set(box, { rotationY: "30" });
+  })
