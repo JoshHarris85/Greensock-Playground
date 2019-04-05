42182fc1df6e57c47adbb93ec8314df8460202f0
JoshHarris85 <joshua.harris1085@gmail.com>
2019-04-05 15:54:42 -0500

    Lesson 3

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 import { TweenMax } from "gsap"
 
-TweenMax.set("#box", { xPercent: -50, yPercent: -50 })
+TweenMax.set("#box", {
+  backgroundColor: "green",
+  width: "50px",
+  height: "50px",
+  x: "50px",
+  y: "50px"
+});
 
-document.addEventListener("click", event => {
-  const { clientX, clientY } = event
-
-  TweenMax.to("#box", 1, { x: clientX, y: clientY })
-})
+document.addEventListener("click", () => {
+  TweenMax.to("#box", 0.5, {
+    rotation: "+=30"
+  });
+});
