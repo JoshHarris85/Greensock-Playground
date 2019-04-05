b7a70e31045f05b314552df03b054846a2f086dc
JoshHarris85 <joshua.harris1085@gmail.com>
2019-04-05 16:14:16 -0500

    Lesson 7

--- index.js
+++ index.js
@@ -8,27 +8,7 @@ TweenMax.set("#box", {
   y: "50px"
 });
 
-const timeline = new TimelineMax({ repeat: -1 });
-
-timeline.pause();
-
-timeline.to("#box", 0.5, { x: 100 });
-timeline.to("#box", 0.5, { y: 100 });
-timeline.to("#box", 0.5, { x: 50 });
-timeline.to("#box", 0.5, { y: 50 });
-
-document.querySelector("#box").addEventListener("click", () => {
-  if (timeline.isActive()) {
-    timeline.pause();
-  } else {
-    timeline.resume();
-  }
-});
-
-document.addEventListener("wheel", (event) => {
-  if (event.wheelDelta > 0) {
-    TweenMax.to(timeline, 0.25, { progress: "+=0.1" })
-  } else {
-    TweenMax.to(timeline, 0.25, { progress: "-=0.1" })
-  }
+document.addEventListener("click", (event) => {
+  const { x, y } = event;
+  TweenMax.fromTo("#box", 1, { x, y }, { x: 150, y: 150});
 });
